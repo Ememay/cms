bb74894701adfc7bfd2cfbf8f8f80245b857dd26
ememay <2001aghakhani@gmail.com>
2022-01-28 12:42:12 +0330

    6

--- index.php
+++ index.php
@@ -1,16 +1,32 @@
 <?php
 
 use Core\Config;
+use Core\ss;
 
 session_start();
 
 //define constans
-define('PROOT',__DIR__);
-define('DS',DIRECTORY_SEPARATOR);
+define('PROOT', __DIR__);
+define('DS', DIRECTORY_SEPARATOR);
 
 
 
-include_once('core/Config.php');
+//autoloader
+spl_autoload_register('myautoloader');
+function myautoloader($className)
+{
 
-$test = new Config;
-echo $test->get('default_layout');
+    $parts = explode('\\' , $className);
+    $class = end($parts);
+    array_pop($parts);
+    $path = strtolower(implode(DS,$parts));
+    $path = PROOT . DS . $path . DS . $class . '.php';
+    if(file_exists($path)){
+        include($path);
+    }
+}
+
+
+
+
+echo Config::get('db_name');
